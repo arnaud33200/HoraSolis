fb193de1affa83cd5e7131c1cb7bdafedce54be3
arnaud33200 <arnaud.ladoucette@joist.com>
2025-07-16 10:48:38 -0400

    update libs + fix text field

--- app/src/main/java/ca/arnaud/horasolis/ui/common/HoraTextField.kt
+++ app/src/main/java/ca/arnaud/horasolis/ui/common/HoraTextField.kt
@@ -1,14 +1,24 @@
 package ca.arnaud.horasolis.ui.common
 
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsFocusedAsState
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.input.TextFieldState
+import androidx.compose.foundation.text.selection.LocalTextSelectionColors
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.TextFieldColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.Stable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.takeOrElse
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
@@ -21,27 +31,60 @@ fun HoraTextField(
     modifier: Modifier = Modifier,
     singleLine: Boolean = true,
     enabled: Boolean = true,
+    isError: Boolean = false,
     visualTransformation: VisualTransformation = VisualTransformation.None,
+    colors: TextFieldColors = OutlinedTextFieldDefaults.colors(),
 ) {
     val interactionSource = remember { MutableInteractionSource() }
-    BasicTextField(
-        state = state,
-        modifier = modifier,
-        decorator = @Composable { innerTextField ->
-            TextFieldDefaults.DecorationBox(
-                value = state.text.toString(),
-                innerTextField = innerTextField,
-                enabled = enabled,
-                singleLine = singleLine,
-                visualTransformation = visualTransformation,
-                label = { Text(label) },
-                contentPadding = TextFieldDefaults.contentPaddingWithoutLabel(
-                    start = 16.dp, top = 8.dp, end = 16.dp, bottom = 8.dp
-                ),
-                interactionSource = interactionSource,
-            )
-        }
-    )
+    val textStyle = MaterialTheme.typography.bodyLarge
+    val textColor = textStyle.color.takeOrElse {
+        val focused = interactionSource.collectIsFocusedAsState().value
+        colors.textColor(enabled, isError, focused)
+    }
+    val mergedTextStyle = textStyle.merge(TextStyle(color = textColor))
+    CompositionLocalProvider(
+        LocalTextSelectionColors provides colors.textSelectionColors
+    ) {
+
+        BasicTextField(
+            state = state,
+            modifier = modifier,
+            textStyle = mergedTextStyle,
+            decorator = @Composable { innerTextField ->
+                OutlinedTextFieldDefaults.DecorationBox(
+                    value = state.text.toString(),
+                    visualTransformation = visualTransformation,
+                    innerTextField = innerTextField,
+                    label = {
+                        Text(text = label)
+                    },
+                    singleLine = singleLine,
+                    enabled = enabled,
+                    isError = isError,
+                    interactionSource = interactionSource,
+                    container = {
+                        OutlinedTextFieldDefaults.Container(
+                            enabled = enabled,
+                            isError = isError,
+                            interactionSource = interactionSource,
+                        )
+                    }
+                )
+            }
+        )
+    }
+}
+
+@Stable
+internal fun TextFieldColors.textColor(
+    enabled: Boolean,
+    isError: Boolean,
+    focused: Boolean,
+): Color = when {
+    !enabled -> disabledTextColor
+    isError -> errorTextColor
+    focused -> focusedTextColor
+    else -> unfocusedTextColor
 }
 
 @PreviewLightDark
@@ -51,6 +94,7 @@ private fun HoraTextFieldPreview() {
         Surface {
             val state = TextFieldState(initialText = "48.858844")
             HoraTextField(
+                modifier = Modifier.padding(12.dp),
                 state = state,
                 label = "Latitude"
             )
