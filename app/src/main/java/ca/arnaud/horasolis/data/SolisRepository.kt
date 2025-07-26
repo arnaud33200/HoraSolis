402ea75557be3ba8b0752168897406774ba97261
arnaud33200 <arnaud.ladoucette@joist.com>
2025-07-26 09:10:19 -0400

    cache solis day response

--- app/src/main/java/ca/arnaud/horasolis/data/SolisRepository.kt
+++ app/src/main/java/ca/arnaud/horasolis/data/SolisRepository.kt
@@ -3,6 +3,7 @@ package ca.arnaud.horasolis.data
 import ca.arnaud.horasolis.domain.Response
 import ca.arnaud.horasolis.domain.map
 import ca.arnaud.horasolis.domain.model.SolisDay
+import ca.arnaud.horasolis.domain.onSuccess
 import ca.arnaud.horasolis.domain.usecase.GetSolisDayParams
 import ca.arnaud.horasolis.remote.KtorClient
 import ca.arnaud.horasolis.remote.model.GetSunTime
@@ -13,9 +14,16 @@ class SolisRepository(
     private val ktorClient: KtorClient
 ) {
 
+    private val cacheSolisDayMap = mutableMapOf<String, SolisDay>()
+
     suspend fun getSolisDay(
         params: GetSolisDayParams,
     ): Response<SolisDay, Throwable> {
+        val cacheKey = params.toCacheKey()
+        cacheSolisDayMap[cacheKey]?.let { cachedSolisDay ->
+            return Response.Success(cachedSolisDay)
+        }
+
         val resource = GetSunTime(
             lat = params.location.lat,
             lng = params.location.lng,
@@ -30,6 +38,12 @@ class SolisRepository(
                     civilSunsetTime = remoteSunTimeResponse.results.sunset,
                 )
             }
-        )
+        ).onSuccess { solisDay ->
+            cacheSolisDayMap[cacheKey] = solisDay
+        }
+    }
+
+    private fun GetSolisDayParams.toCacheKey(): String {
+        return "${location.lat},${location.lng},${location.timZoneId},${date.toIsoString()}"
     }
 }
