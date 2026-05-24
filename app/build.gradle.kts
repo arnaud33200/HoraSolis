31908aa76639dd2a433c1ce4390b5e772992059d
arnaud33200 <arnaud.ladoucette@joist.com>
2026-05-24 10:08:31 -0400

    2.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 1
-        versionName = "2.3"
+        versionName = "2.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
